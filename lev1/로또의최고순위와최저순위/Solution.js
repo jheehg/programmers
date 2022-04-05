419d27fa85ba7f71cc417926a80fed7922d2cd44
Ju hee Hong <juhee.hg@gmail.com>
2022-04-05 22:00:02 +0900

    lev1/로또의최고순위와최저순위/Solution.js

---
+++ lev1/로또의최고순위와최저순위/Solution.js
@@ -0,0 +1,13 @@
+function solution(lottos, win_nums) {
+    let check = 6;
+    let zero = 0;
+    lottos.forEach(v => {
+        if(!win_nums.includes(v)) {
+            if(v === 0) zero++;
+            check--;
+        }
+    });
+    let max = (check + zero) < 2? 1 : check + zero;
+    let min = check < 2? 1: check;
+    return [7 - max, 7 - min];
+}
